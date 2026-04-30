3b38b9ed1b9d5432d4e6788ed8e4ebd48560b446
Yh4no <felipe170f@gmail.com>
2026-04-30 15:08:49 -0300

    update 1.1
    I updated the look of some pages, such as colors, the "about me" description, etc.

--- life.js
+++ life.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btnSobre = document.querySelector(".button1");
-  const btnProjetos = document.querySelector(".button2");
+  const btnProjetos =document.querySelector(".button2");
   const btnContato = document.querySelector(".button3");
+  const btnskills = document.querySelector(".button4");
+
 
   btnSobre.addEventListener("click", () => {
     window.location.href = "index.html";
@@ -14,6 +16,12 @@ document.addEventListener("DOMContentLoaded", () => {
   btnContato.addEventListener("click", () => {
     window.location.href = "contato.html";
   });
+
+   btnskills.addEventListener("click", () => {
+    window.location.href = "skills.html";
+  });
+
+
 });
 
   
